9f0eae3b26f53da7b040ba90dfe3302afc1ae250
daekwon yoo <yoosw00@gmail.com>
2026-03-07 16:13:43 +0900

    feat: Swagger UI JWT 인증 설정 추가
    - OpenApiConfig에 Bearer Authentication 스키마 추가
    - Swagger UI에서 Authorize 버튼으로 JWT 토큰 입력 가능

---
+++ src/main/java/kr/or/study/springcqrs/config/OpenApiConfig.java
@@ -0,0 +1,31 @@
+package kr.or.study.springcqrs.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        String securitySchemeName = "Bearer Authentication";
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Spring CQRS Intranet API")
+                        .version("1.0")
+                        .description("인트라넷 시스템 REST API"))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+}
